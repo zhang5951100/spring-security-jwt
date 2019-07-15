d2e77707ccea72a323a2deadff8276f56cfb1bf5
286594635@qq.com <zhang0022>
2019-07-15 16:59:48 +0800

    配置异常处理

---
+++ src/main/java/com/izuul/springsecurity/exception/MyException.java
@@ -0,0 +1,66 @@
+package com.izuul.springsecurity.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author Guihong.Zhang
+ * @date 2019-07-14 09:47
+ */
+public class MyException extends Exception {
+
+	private static final long serialVersionUID = 1926439629486459955L;
+
+	private HttpStatus httpStatus;
+	private String solution;
+
+	/**
+	 *
+	 */
+	public MyException() {
+
+		super();
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public MyException(String message, Throwable cause) {
+
+		super(message, cause);
+	}
+
+	/**
+	 *
+	 * @param message
+	 * @param cause
+	 * @param httpStatus
+	 */
+	public MyException(String message, Throwable cause, HttpStatus httpStatus) {
+
+		super(message, cause);
+		this.httpStatus = httpStatus;
+	}
+
+
+	/**
+	 * @param message
+	 */
+	public MyException(String message) {
+
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public MyException(Throwable cause) {
+
+		super(cause);
+	}
+
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+}
---
+++ src/main/java/com/izuul/springsecurity/exception/MyExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.izuul.springsecurity.exception;
+
+import com.izuul.springsecurity.controller.vo.CodeEnum;
+import com.izuul.springsecurity.controller.vo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Guihong.Zhang
+ * @date 2019-07-14 09:47
+ */
+@ControllerAdvice
+@Slf4j
+public class MyExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Object kunlunExceptionHandler(Exception e) {
+
+        log.error("kunlunExceptionHandler:{}", e.getMessage());
+        return new ResponseEntity<>(Result.builder()
+                .code(CodeEnum.SUCCESS.getCode())
+                .message(CodeEnum.SUCCESS.getMsg())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
